a716a24a53c061163b762e70c18d31ebc12ea3a8
variphx <variphx@gmail.com>
2025-08-20 18:44:57 +0700

    add/fix: add qdrant_client to AppState and refactor AppState::new

--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,27 +1,48 @@
+use std::sync::Arc;
+
 use deadpool_diesel::{
     Runtime,
     postgres::{Manager, Pool},
 };
+use qdrant_client::Qdrant;
 
 #[derive(Clone)]
 pub struct AppState {
-    pool: Pool,
+    diesel_pool: Pool,
+    qdrant_client: Arc<Qdrant>,
 }
 
 impl AppState {
     pub fn new() -> anyhow::Result<Self> {
         Ok(Self {
-            pool: Pool::builder(Manager::new(
-                std::env::var("DATABASE_URL")?,
-                Runtime::Tokio1,
-            ))
-            .build()?,
+            diesel_pool: Self::diesel_pool_helper()?,
+            qdrant_client: Self::qdrant_client_helper()?,
         })
+    }
+
+    fn diesel_pool_helper() -> anyhow::Result<Pool> {
+        Pool::builder(Manager::new(
+            std::env::var("DATABASE_URL")?,
+            Runtime::Tokio1,
+        ))
+        .build()
+        .map_err(Into::into)
+    }
+
+    fn qdrant_client_helper() -> anyhow::Result<Arc<Qdrant>> {
+        Qdrant::from_url(&std::env::var("QDRANT_URL")?)
+            .build()
+            .map(Arc::new)
+            .map_err(Into::into)
     }
 }
 
 impl AppState {
     pub fn pool(&self) -> &Pool {
-        &self.pool
+        &self.diesel_pool
+    }
+
+    pub fn qdrant_client(&self) -> &Qdrant {
+        &self.qdrant_client
     }
 }
